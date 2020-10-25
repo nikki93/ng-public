4312cdc90ba65d9133648c7a030411cc66fce08b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-10-24 19:47:16 -0700

    focus run script

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ case "$1" in
 
   # Format
   format)
-    clang-format -i -style=file src/engine/* src/game/* src/main.cc
+    clang-format -i -style=file src/*.h src/*.c
     ;;
 
   # Desktop
@@ -35,79 +35,63 @@ case "$1" in
         $CMAKE --build build/release
         ./build/release/ng
         ;;
-      win)
-        $CMAKE -H. -Bbuild/msvc -G"Visual Studio 16"
-        $CMAKE --build build/msvc --config Release
-        ./build/msvc/Release/ng.exe 
-        ;;
+#      win)
+#        $CMAKE -H. -Bbuild/msvc -G"Visual Studio 16"
+#        $CMAKE --build build/msvc --config Release
+#        ./build/msvc/Release/ng.exe 
+#        ;;
     esac
     ;;
-  debug)
-    case $PLATFORM in
-      lin|macOS)
-        $CMAKE -DCMAKE_BUILD_TYPE=Debug -H. -Bbuild/debug -GNinja
-        $CMAKE --build build/debug
-        ./build/debug/ng
-        ;;
-      win)
-        $CMAKE -H. -Bbuild/msvc -G"Visual Studio 16"
-        $CMAKE --build build/msvc --config Debug
-        ./build/msvc/Debug/ng.exe 
-        ;;
-    esac
-    ;;
-  lib-release)
-    case $PLATFORM in
-      lin|macOS)
-        $CMAKE -DLIB=ON -H. -Bbuild/lib-release -GNinja
-        $CMAKE --build build/lib-release
-        ;;
-      win)
-        $CMAKE -DLIB=ON -H. -Bbuild/lib-msvc -G"Visual Studio 16"
-        $CMAKE --build build/lib-msvc --config Release
-        ;;
-    esac
-    ;;
+#  debug)
+#    case $PLATFORM in
+#      lin|macOS)
+#        $CMAKE -DCMAKE_BUILD_TYPE=Debug -H. -Bbuild/debug -GNinja
+#        $CMAKE --build build/debug
+#        ./build/debug/ng
+#        ;;
+#      win)
+#        $CMAKE -H. -Bbuild/msvc -G"Visual Studio 16"
+#        $CMAKE --build build/msvc --config Debug
+#        ./build/msvc/Debug/ng.exe 
+#        ;;
+#    esac
+#    ;;
 
   # Mobile
-  ios-debug)
-    $CMAKE -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=6PUB3623F7 -H. -Bbuild/ios -GXcode
-    cmake --build build/ios --config Debug
-    ios-deploy --no-wifi --debug --bundle build/ios/Debug-iphoneos/ng.app
-    ;;
-  ios-release)
-    $CMAKE -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=6PUB3623F7 -H. -Bbuild/ios -GXcode
-    cmake --build build/ios --config Release
-    ios-deploy --no-wifi --justlaunch --bundle build/ios/Release-iphoneos/ng.app
-    ;;
+#  ios-debug)
+#    $CMAKE -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=6PUB3623F7 -H. -Bbuild/ios -GXcode
+#    cmake --build build/ios --config Debug
+#    ios-deploy --no-wifi --debug --bundle build/ios/Debug-iphoneos/ng.app
+#    ;;
+#  ios-release)
+#    $CMAKE -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=6PUB3623F7 -H. -Bbuild/ios -GXcode
+#    cmake --build build/ios --config Release
+#    ios-deploy --no-wifi --justlaunch --bundle build/ios/Release-iphoneos/ng.app
+#    ;;
 
   # Web
   web-release)
     $CMAKE -DWEB=ON -H. -Bbuild/web-release -GNinja
     $CMAKE --build build/web-release
     ;;
-  web-debug)
-    $CMAKE -DCMAKE_BUILD_TYPE=Debug -DWEB=ON -H. -Bbuild/web-debug -GNinja
-    $CMAKE --build build/web-debug
-    ;;
-  web-lib-release)
-    $CMAKE -DLIB=ON -DWEB=ON -H. -Bbuild/web-lib-release -GNinja
-    $CMAKE --build build/web-lib-release
-    ;;
+#  web-debug)
+#    $CMAKE -DCMAKE_BUILD_TYPE=Debug -DWEB=ON -H. -Bbuild/web-debug -GNinja
+#    $CMAKE --build build/web-debug
+#    ;;
   web-watch-release)
     find CMakeLists.txt src web -type f | entr ./run.sh web-release
     ;;
-  web-watch-debug)
-    find CMakeLists.txt src web -type f | entr ./run.sh web-debug
-    ;;
+#  web-watch-debug)
+#    find CMakeLists.txt src web -type f | entr ./run.sh web-debug
+#    ;;
   web-serve-release)
     npx http-server -c-1 build/web-release
     ;;
-  web-serve-debug)
-    npx http-server -c-1 build/web-debug
-    ;;
-  web-publish)
-    ./run.sh web-release
-    rsync -avP build/web-release/{index.*,ng.*} # TODO(nikki): add host here...
-    ;;
+#  web-serve-debug)
+#    npx http-server -c-1 build/web-debug
+#    ;;
+#  web-publish)
+#    ./run.sh web-release
+#    rsync -avP build/web-release/{index.*,ng.*} # TODO(nikki): add host here...
+#    ;;
 esac
