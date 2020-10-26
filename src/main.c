142a8d4775701e6d87f6d783bef27e237661e33d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-10-25 17:33:49 -0700

    remove fib benchmark

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int cFib(int n) {
-  if (n <= 1) {
-    return 1;
-  } else {
-    return cFib(n - 1) + cFib(n - 2);
-  }
-}
--- web/index.nim
+++ web/index.nim
@@ -1,12 +1,1 @@
-import times
-
-proc fib(n: int): int =
-  if n <= 1:
-    1
-  else:
-    fib(n - 1) + fib(n - 2)
-
-let before = getTime()
-let res = fib(42)
-let after = getTime()
-echo("nim-js: res: ", res, ", took: ", (after - before).inMilliseconds(), "ms")
+echo "hello from nim-js!"
