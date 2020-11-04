eb79c2fb1561772768dc00efa986927e2a7d7eef
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-11-03 22:15:22 -0800

    more contrast for touch rectangles

--- src/main.nim
+++ src/main.nim
@@ -17,9 +17,9 @@ ev.loop:
     let r = 0xff * (0.2 * (sin(2 * tim.t) + 1) + 0.1)
     let rU8 = cast[uint8](r.toInt)
     if ev.touches.len > 0:
-      gfx.clear(rU8, 0x80, 0x20)
+      gfx.clear(rU8, 0x20, 0x20)
     else:
-      gfx.clear(rU8, 0x20, 0x80)
+      gfx.clear(rU8, 0x80, 0x20)
 
     gfx.drawLine(100, 100, 400, 300)
     gfx.scope:
