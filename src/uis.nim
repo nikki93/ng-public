c8a945933dfc3777620fe7887fae6f503f284e3c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-11-08 22:10:11 -0800

    fix up conditional inclusion of ui code in emscripten

--- src/uis.nim
+++ src/uis.nim
@@ -1,7 +1,9 @@
 ## Interface to DOM-based user interface in the browser. All operations
 ## no-op cleanly on non-browser platforms.
 
-import std/[macros, strutils]
+import std/macros
+when defined(emscripten):
+  import std/strutils
 
 import utils
 
@@ -28,52 +30,31 @@ when defined(emscripten):
   proc JS_uiAttrBool(name: cstring, value: bool) {.importc.}
   proc JS_uiAttrStr(name: cstring, value: cstring) {.importc.}
   proc JS_uiAttrClass(value: cstring) {.importc.}
-else:
-  proc JS_uiElemOpenStart(tag: cstring) = discard
-  proc JS_uiElemOpenStartKeyInt(tag: cstring, key: int) = discard
-  proc JS_uiElemOpenStartKeyStr(tag: cstring, key: cstring) = discard
 
-  proc JS_uiElemOpenEnd() = discard
+  proc elemOpenStart(ui: var UI, tag: string) =
+    JS_uiElemOpenStart(tag)
+  proc elemOpenStart(ui: var UI, tag: string, key: int) =
+    JS_uiElemOpenStartKeyInt(tag, key)
+  proc elemOpenStart(ui: var UI, tag: string, key: string) =
+    JS_uiElemOpenStartKeyStr(tag, key)
 
-  proc JS_uiElemClose(tag: cstring) = discard
+  proc elemOpenEnd(ui: var UI) =
+    JS_uiElemOpenEnd()
 
-  proc JS_uiAttrInt(name: cstring, value: int) = discard
-  proc JS_uiAttrFloat(name: cstring, value: float) = discard
-  proc JS_uiAttrBool(name: cstring, value: bool) = discard
-  proc JS_uiAttrStr(name: cstring, value: cstring) = discard
-  proc JS_uiAttrClass(value: cstring) = discard
+  proc elemClose(ui: var UI, tag: string) =
+    JS_uiElemClose(tag)
 
+  proc attr(ui: var UI, name: string, value: int) =
+    JS_uiAttrInt(name, value)
+  proc attr(ui: var UI, name: string, value: float) =
+    JS_uiAttrFloat(name, value)
+  proc attr(ui: var UI, name: string, value: bool) =
+    JS_uiAttrBool(name, value)
+  proc attr(ui: var UI, name: string, value: string) =
+    JS_uiAttrStr(name, value)
 
-proc elemOpenStart(ui: var UI, tag: string) =
-  JS_uiElemOpenStart(tag)
-
-proc elemOpenStart(ui: var UI, tag: string, key: int) =
-  JS_uiElemOpenStartKeyInt(tag, key)
-
-proc elemOpenStart(ui: var UI, tag: string, key: string) =
-  JS_uiElemOpenStartKeyStr(tag, key)
-
-proc elemOpenEnd(ui: var UI) =
-  JS_uiElemOpenEnd()
-
-proc elemClose(ui: var UI, tag: string) =
-  JS_uiElemClose(tag)
-
-proc attr(ui: var UI, name: string, value: int) =
-  JS_uiAttrInt(name, value)
-
-proc attr(ui: var UI, name: string, value: float) =
-  JS_uiAttrFloat(name, value)
-
-proc attr(ui: var UI, name: string, value: bool) =
-  JS_uiAttrBool(name, value)
-
-proc attr(ui: var UI, name: string, value: string) =
-  JS_uiAttrStr(name, value)
-
-proc class(ui: var UI, value: string) =
-  JS_uiAttrClass(value)
-
+  proc class(ui: var UI, value: string) =
+    JS_uiAttrClass(value)
 
 macro elem*(ui: var UI, tag: string, args: varargs[untyped]) =
   ## Display a DOM-based UI element. A `tag` is always required and is the
@@ -87,60 +68,63 @@ macro elem*(ui: var UI, tag: string, args: varargs[untyped]) =
   ##  - A statement list, which is run "inside" the element. Elements
   ##    displayed inside become children of the outer element.
 
-  const debug = false
-
-  when debug:
-    for i, arg in args:
-      echo "arg[", i, "]:\n", arg.treeRepr, "\n"
-
-  result = newStmtList()
-
-  # Check for key
-  var key: NimNode
-  for node in args:
-    if node.kind in {nnkExprColonExpr, nnkExprEqExpr} and $node[0] == "key":
-      key = node[1]
-
-  # Start open
-  if key != nil:
-    result.add quote do:
-      elemOpenStart(`ui`, `tag`, `key`)
+  when not defined(emscripten):
+    result = newStmtList()
   else:
-    result.add quote do:
-      elemOpenStart(`ui`, `tag`)
+    const debug = false
 
-  # Attributes
-  for node in args:
-    case node.kind:
-    of nnkStrLit:
+    when debug:
+      for i, arg in args:
+        echo "arg[", i, "]:\n", arg.treeRepr, "\n"
+
+    result = newStmtList()
+
+    # Check for key
+    var key: NimNode
+    for node in args:
+      if node.kind in {nnkExprColonExpr, nnkExprEqExpr} and $node[0] == "key":
+        key = node[1]
+
+    # Start open
+    if key != nil:
       result.add quote do:
-        class(`ui`, `node`)
-    of nnkExprColonExpr, nnkExprEqExpr:
-      if $node[0] != "key":
-        let name = newLit($node[0])
-        let value = node[1]
-        result.add quote do:
-          attr(`ui`, `name`, `value`)
-    of nnkStmtList:
-      discard
+        elemOpenStart(`ui`, `tag`, `key`)
     else:
-      error("`ui.elem` parameters must be attributes or a body", node)
+      result.add quote do:
+        elemOpenStart(`ui`, `tag`)
 
-  # End open
-  result.add quote do:
-    elemOpenEnd(`ui`)
+    # Attributes
+    for node in args:
+      case node.kind:
+      of nnkStrLit:
+        result.add quote do:
+          class(`ui`, `node`)
+      of nnkExprColonExpr, nnkExprEqExpr:
+        if $node[0] != "key":
+          let name = newLit($node[0])
+          let value = node[1]
+          result.add quote do:
+            attr(`ui`, `name`, `value`)
+      of nnkStmtList:
+        discard
+      else:
+        error("`ui.elem` parameters must be attributes or a body", node)
 
-  # Inside
-  if args.len > 0 and args.last.kind == nnkStmtList:
-    result.add(args.last)
+    # End open
+    result.add quote do:
+      elemOpenEnd(`ui`)
 
-  # Close
-  result.add quote do:
-    elemClose(`ui`, `tag`)
+    # Inside
+    if args.len > 0 and args.last.kind == nnkStmtList:
+      result.add(args.last)
 
-  when debug:
-    echo "tree: ", result.treeRepr, "\n"
-    echo "code: ", result.repr, "\n"
+    # Close
+    result.add quote do:
+      elemClose(`ui`, `tag`)
+
+    when debug:
+      echo "tree: ", result.treeRepr, "\n"
+      echo "code: ", result.repr, "\n"
 
 
 # Common elements
@@ -156,53 +140,48 @@ template button*(ui: var UI, args: varargs[untyped]) =
 
 # Text
 
-when defined(emscripten):
-  proc JS_uiText(value: cstring) {.importc.}
-else:
-  proc JS_uiText(value: cstring) = discard
-
 proc text*(ui: var UI, value: string) {.inline.} =
   ## Add the given text as a child of the enclosing UI element.
-  JS_uiText(value)
+  when defined(emscripten):
+    proc JS_uiText(value: cstring) {.importc.}
+    JS_uiText(value)
 
 
 # Events
 
 when defined(emscripten):
   proc JS_uiEventCount(typ: cstring): int {.importc.}
-else:
-  proc JS_uiEventCount(typ: cstring): int = discard
 
 template event*(ui: var UI, eventType: string, body: typed) =
   ## Run the body for each occurrence of the named event in the last
   ## UI frame for the enclosing UI element.
-  block:
-    let count = JS_uiEventCount(eventType)
-    for i in 1..count:
-      body
+  when defined(emscripten):
+    block:
+      let count = JS_uiEventCount(eventType)
+      for i in 1..count:
+        body
 
 
 # Values
 
-when defined(emscripten):
-  proc JS_uiValue(): cstring {.importc.}
-else:
-  proc JS_uiValue(): cstring = discard
-
 proc valueStr*(ui: var UI): string =
   ## Get the value of the current UI element as a string.
-  let cStr = JS_uiValue()
-  result = $cStr
-  proc free(p: pointer) {.importc.}
-  free(cStr)
+  when defined(emscripten):
+    proc JS_uiValue(): cstring {.importc.}
+    let cStr = JS_uiValue()
+    result = $cStr
+    proc free(p: pointer) {.importc.}
+    free(cStr)
 
 proc valueInt*(ui: var UI): int =
   ## Get the value of the current UI element as an integer.
-  ui.valueStr.parseInt
+  when defined(emscripten):
+    ui.valueStr.parseInt
 
 proc valueFloat*(ui: var UI): float =
   ## Get the value of the current UI element as a floating point number.
-  ui.valueStr.parseFloat
+  when defined(emscripten):
+    ui.valueStr.parseFloat
 
 
 # Patch
@@ -211,17 +190,16 @@ var thePatchProc: proc()
 
 when defined(emscripten):
   proc JS_uiPatch(id: cstring) {.importc.}
-else:
-  proc JS_uiPatch(id: cstring) = discard
 
 template patch*(ui: var UI, id: cstring, body: typed) =
   ## Display the UI elements in the body at the DOM node with the given ID.
-  block:
-    proc patchProc() =
-      body
-    thePatchProc = patchProc
-    JS_uiPatch(id)
-    thePatchProc = nil
+  when defined(emscripten):
+    block:
+      proc patchProc() =
+        body
+      thePatchProc = patchProc # Save the patch proc to be called below
+      JS_uiPatch(id)
+      thePatchProc = nil
 
 when defined(emscripten):
   proc JS_uiCallPatchProc()
