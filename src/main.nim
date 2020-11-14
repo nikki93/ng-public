1fa925246afef0048236e2ab9cf1dad9a7add0ec
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-11-14 15:43:01 -0800

    basic player feet with walking :O

--- src/main.nim
+++ src/main.nim
@@ -3,10 +3,9 @@ import std/math
 import ng
 
 
-
-# Types
-
 type
+  # Basics
+
   Position {.ng.} = object
     x, y: float
 
@@ -19,6 +18,27 @@ type
     #flipH: bool
 
 
+  # Physics
+
+  Feet {.ng.} = object
+    body: Body
+    shape: Shape
+    offsetX, offsetY: float
+
+  Walk {.ng.} = object
+    target: Body
+    constr: Constraint
+    touchTime: float
+
+  Friction {.ng.} = object
+    constr: Constraint
+
+
+  # Player
+
+  Player {.ng.} = object
+
+
 # Triggers
 
 type Trigger = seq[proc()]
@@ -27,7 +47,11 @@ proc run(s: Trigger) =
   for fn in s:
     fn()
 
+var onPhysicsPre: Trigger
+var onPhysicsPost: Trigger
+
 var onDraw: Trigger
+var onDrawOverlay: Trigger
 
 
 # Sprite
@@ -37,6 +61,46 @@ onDraw.add proc() =
     a.depth < b.depth)
   for _, spr, pos in ker.each(Sprite, Position):
     gfx.drawImage(spr.image, pos.x, pos.y, spr.scale)
+
+
+# Player
+
+onPhysicsPre.add proc() =
+  # Walk player to touch
+  if ev.touches.len > 0:
+    let touch = ev.touches[0]
+    for ent, _, feet in ker.each(Player, Feet):
+      var walk = ker.get(Walk, ent)
+      if walk == nil:
+        walk = ker.add(Walk, ent)
+        walk.target = phy.createStatic()
+        walk.constr = phy.createPivot(walk.target, feet.body)
+        walk.constr.maxForce = 2000
+        walk.constr.maxBias = 180
+      walk.target.position = (touch.x, touch.y)
+      walk.touchTime = tim.t
+
+onPhysicsPost.add proc() =
+  # Remove walk if reached target or obstructed
+  discard
+
+onPhysicsPost.add proc() =
+  # Read player physics
+  for _, _, feet, pos in ker.each(Player, Feet, Position):
+    let (x, y) = feet.body.position
+    (pos.x, pos.y) = (x - feet.offsetX, y - feet.offsetY)
+
+onPhysicsPost.add proc() =
+  # Set player depth behind objects that obscure it
+  discard
+
+onDrawOverlay.add proc() =
+  # Draw player footprints at target when walking
+  discard
+
+onPhysicsPost.add proc() =
+  # Update player animation based on walk state
+  discard
 
 
 # main
@@ -55,18 +119,33 @@ proc main() =
     spr.scale = 0.25
     spr.depth = 1000
 
+    let feet = ker.add(Feet, ent)
+    feet.body = phy.createDynamic(1, Inf)
+    feet.body.entity = ent
+    feet.offsetY = 65.0
+    feet.body.position = (pos.x, pos.y + feet.offsetY)
+    const rad = 8.0
+    feet.shape = phy.createBox(feet.body, 45 - 2 * rad, 20 - 2 * rad, rad)
+
+    let fric = ker.add(Friction, ent)
+    fric.constr = phy.createPivot(phy.getBackground(), feet.body)
+    fric.constr.maxForce = 800
+    fric.constr.maxBias = 0
+
+    discard ker.add(Player, ent)
+
   ev.loop:
     tim.frame()
-    if tim.dt >= 3 * 1 / 60.0: # Frame drop
+    if tim.dt >= 3 * 1 / 60.0 or not ev.windowFocused:
       return
 
-    if not ev.windowFocused:
-      return
-
+    onPhysicsPre.run()
     phy.frame()
+    onPhysicsPost.run()
 
     gfx.frame:
       onDraw.run()
+      onDrawOverlay.run()
 
     ui.frame:
       ui.patch("bottom"):
