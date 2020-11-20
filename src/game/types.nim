254e95ef08779ef7c9e9125cf1f4a2f095f79a5c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-11-19 17:23:21 -0800

    moar commantz

--- src/game/types.nim
+++ src/game/types.nim
@@ -1,14 +1,15 @@
+## Defines all of the types that can be added to entities in the game.
+##
+## These are in one file so the game's state structure is understandable
+## at a glance, and for easy import into other modules.
+
 import ng
 
-
-# Annotations
 
 template nosave() {.pragma.} # Skip save / load for type
 
 template noedit() {.pragma.} # Skip display of type in editor UI
 
-
-## All of the types that can be added to entities in the game.
 
 type
   # Basics
